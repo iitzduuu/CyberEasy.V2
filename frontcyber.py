cca61f8ec4675857ee7d5f3139f43cdc61575a6e
iitzduuu <coutinhoeduardo192@gmail.com>
2025-08-22 19:05:25 -0300

    feat:adiciona logica de upload

--- frontcyber.py
+++ frontcyber.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import time
-st.title('CYBEREASY')#nome pode ser alterado
-st.write('Welcome to cybereasy  ')
 
-file = st.file_uploader("Drag a problaly malicious file here") #caixa para selecionar arquivo
+st.title('CYBEREASY')  # nome pode ser alterado
+st.write('Welcome to cybereasy')
 
-progress_bar = st.progress(0) #barra de progresso
-for percent_complete in range(101):
-    time.sleep(0.05)  
-    progress_bar.progress(percent_complete)
-with st.spinner('Loading files... '):
-    time.sleep(2)
-st.success('Completed')
+uploader_file = st.file_uploader("Drag a probably malicious file here")  # caixa para selecionar arquivo
 
+if uploader_file is not None:
+    if st.button('Analyze file'):
+        # Barra de progresso
+        progress_bar = st.progress(0)
+        for percent_complete in range(101):
+            time.sleep(0.05)
+            progress_bar.progress(percent_complete)
+
+        # Spinner
+        with st.spinner('Loading file...'):
+            time.sleep(2)
+
+        st.success('Is not a virus')
+#python -m streamlit run frontcyber.py
+#  #python -m pip install streamlit
